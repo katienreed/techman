9db40b04d38f9c38aa592eccfdae0ed72e90a2a2
katie.reed@me.com <katie.reed@me.com>
2014-12-09 19:16:51 -0500

    Added a profile route, controller and modified the css of the page to display the user's information placements and created the scaffolding for a score board

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -83,6 +83,29 @@ $("span").on("click", function(){
 
 
 // This selects the noose and animates it's entrance on the homepage
-$(document).ready(function () {
-  $("#noose").animate({ "bottom": "-190px" }, "slow" );
+//$(document).ready(function () {
+//  $("#noose").animate({ "bottom": "-260px" }, "slow" );
+//});
+
+// This selects the noose to swing back and forth
+$(document).ready(function() {
+  right();
+
+  function right() {
+    $('#noose').stop().animate({
+      // Move the image right to the width of the container, minus the width of the image
+      left: '200px',
+    }, 5000, function() {
+      left()
+    });
+  }
+  function left() {
+    $('#noose').stop().animate({
+      // Move back to the left
+      left: '150px',
+    }, 5000, function() {
+      right()
+    });
+  }
+
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'profile/index'
+
   get 'home/index'
 
   get "/game"=>"game#index"
 
   get "/home"=>"home#index"
+
+  get "/profile"=>"profile#index"
 end
