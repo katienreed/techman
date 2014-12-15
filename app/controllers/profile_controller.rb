0160ac62b275031c5c10b13f6e4b61c41745e224
katie.reed@me.com <katie.reed@me.com>
2014-12-15 10:43:55 -0500

    meh

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,7 @@
 class ProfileController < ApplicationController
   def index
   	@current_user = User.find(session[:user_id])
+    @users = User.order('wins DESC, :username, :avatar').first(5)
   	if @current_user.games === nil
   		@score = 0
   	else
